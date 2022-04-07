1f434a6a1485678f3f3cd3ead2f6f2f7cc954ed4
yuda-lyu <firsemisphere@gmail.com>
2022-04-07 16:04:22 +0800

    modify: update pks

--- README.md
+++ README.md
@@ -23,5 +23,5 @@ npm i w-statistic
 
 Add script for w-statistic.
 ```alias
-<script src="https://cdn.jsdelivr.net/npm/w-statistic@1.0.1/dist/w-statistic.umd.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/w-statistic@1.0.2/dist/w-statistic.umd.js"></script>
 ```
--- toolg/gDistRollup.mjs
+++ toolg/gDistRollup.mjs
@@ -1,21 +1,56 @@
+import path from 'path'
 import rollupFiles from 'w-package-tools/src/rollupFiles.mjs'
+import rollupWorker from 'w-package-tools/src/rollupWorker.mjs'
 
 
 let fdSrc = './src'
 let fdTar = './dist'
 
-rollupFiles({ //rollupFiles預設會clean folder
-    fns: 'WGeo.mjs',
-    fdSrc,
-    fdTar,
-    nameDistType: 'kebabCase',
-    // bNodePolyfill: true,
-    // bMinify: false,
-    globals: {
-    },
-    external: [
-    ],
-})
+
+async function rp() {
+
+    await rollupFiles({ //rollupFiles預設會clean folder
+        fns: 'WStatistic.mjs',
+        fdSrc,
+        fdTar,
+        nameDistType: 'kebabCase',
+        // bNodePolyfill: true,
+        // bMinify: false,
+        globals: {
+        },
+        external: [
+        ],
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    // await rollupWorker({
+    //     name: 'WStatistic', //原模組名稱, 將來會掛於winodw下
+    //     type: 'object', //原模組輸出為物件
+    //     funNames: ['PCA', 'cluster'],
+    //     // target: 'old', //不支援IE11
+    //     // bNodePolyfill: true,
+    //     // bMinify: false,
+    //     // bSourcemap: true, //rollupWebWorker不提供sourcemap
+    //     execObjectFunsByInstance: true, //各函式使用獨立實體執行
+    //     fpSrc: path.resolve(fdSrc, 'WStatistic.mjs'), //原始檔案路徑
+    //     fpTar: path.resolve(fdTar, 'w-cluster.wk.umd.js'), //檔案輸出路徑
+    //     hookNameDist: () => 'w-cluster',
+    //     // nameDistType: 'kebabCase',
+    //     formatOut: 'umd', //umd為瀏覽器端直接使用, es為供vue-cli或webpack使用
+    //     globals: {
+    //     },
+    //     external: [
+    //     ],
+    // })
+    //     .catch((err) => {
+    //         console.log(err)
+    //     })
+
+}
+rp()
     .catch((err) => {
         console.log(err)
     })
+
